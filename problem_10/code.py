d601256e0ba310d2ad056349f20cbba331bb33b8
Chris Evans <chris.evans@adroll.com>
2020-12-10 10:11:26 -0800

    Delete old code

--- problem_10/code.py
+++ problem_10/code.py
@@ -13,18 +13,5 @@ jolts = [0] + adaptors + [max(adaptors) + 3]
 num_paths = [0] * len(jolts)
 num_paths[-1] = 1
 for i in reversed(range(0, len(jolts)-1)):
-    paths = 0
-    for j in range(i, len(jolts)):
-        if jolts[j] - jolts[i] <= 3:
-            paths += num_paths[j]
-    num_paths[i] = paths
-print(num_paths[0])
-
-# PART 2
-jolts = [0] + adaptors + [max(adaptors) + 3]
-# Dynamic Programming
-num_paths = [0] * len(jolts)
-num_paths[-1] = 1
-for i in reversed(range(0, len(jolts)-1)):
     num_paths[i] = sum(num_paths[j] for j in range(i, len(jolts)) if jolts[j] - jolts[i] <= 3)
 print(num_paths[0])
