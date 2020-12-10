a13ef329187f04b91b7ee5a6cd7a54d589d72533
Chris Evans <chris.evans@adroll.com>
2020-12-09 22:29:51 -0800

    Cleanup Problem 10

--- problem_10/code.py
+++ problem_10/code.py
@@ -11,11 +11,11 @@ print(diffs.count(1) * diffs.count(3))
 jolts = [0] + adaptors + [max(adaptors) + 3]
 # Dynamic Programming
 num_paths = [0] * len(jolts)
-num_paths[0] = 1
-for i in range(1, len(jolts)):
+num_paths[-1] = 1
+for i in reversed(range(0, len(jolts)-1)):
     paths = 0
-    for j in reversed(range(0, i)):
-        if jolts[-j-1] - jolts[-i-1] <= 3:
+    for j in range(i, len(jolts)):
+        if jolts[j] - jolts[i] <= 3:
             paths += num_paths[j]
     num_paths[i] = paths
-print(num_paths[-1])
+print(num_paths[0])
